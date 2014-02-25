5a83ed0d5852c3ac8b695aee53bd16e8016ac440
NaugolnyhN <naugolnyh.n@gmail.com>
2014-02-25 12:49:27 +0200

    c# tasks

---
+++ Abstract class.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(4,2);  //new object of the class Rectangle
+            Console.WriteLine("Square of the rectangle with the sides 2 and 4 is " + rectangle.CalculateSquare());
+            Console.ReadLine();
+        }
+        abstract class Shape
+        {
+            public abstract int CalculateSquare();       //abstract method     
+        }
+
+        class Rectangle : Shape
+        {
+           public Rectangle(int length , int width)
+            {
+                this.length = length;
+                this.width = width;
+            }
+            int length;
+            int width;
+            public override int CalculateSquare()       //overriding method to non-abstract inheritted class
+            {
+                int square = length * width;
+                return square;
+            }
+        }
+    }
+}
---
+++ Polymorphism.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Teacher teacher = new Teacher();
+                int money = 0;
+                teacher.EarnMoney(money);
+                GoodTeacher good_teacher = new GoodTeacher();
+                good_teacher.EarnMoney(money);
+                Console.ReadLine();
+
+        }
+
+
+// 2.Polymorphism example
+        class Teacher
+        {
+            public virtual void EarnMoney(int money)
+            {
+                Console.WriteLine("Teacher earns " + money + " money");
+            }
+        }
+
+        class GoodTeacher : Teacher
+        {
+            public override void EarnMoney(int money)
+            {
+                int bonus = 1;
+                money += bonus;
+                Console.WriteLine("Good teacher earns " + money + " money");
+            }
+        }     
+    }
+}
